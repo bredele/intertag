dbfc5bbbd0aa18b8cd6f8819cef715e3a173bc55
bredele <olivier.wietrich@gmail.com>
2018-07-03 12:57:13 -0600

    Start over

--- index.js
+++ index.js
@@ -1,38 +1,4 @@
 
-
 module.exports = (chunks, ...data) => {
-  const args = []
-  let tail = ''
-  for (var i = 0, l = data.length; i < l; i++) {
-    const item = data[i]
-    if (typeof item === 'function') {
-      const fn = data.splice(i, 1)[0]
-      const results = transform(fn())
-      let templates = [...results.shift()]
-      const head = templates.shift()
-      args.push(tail + chunks[i] + head)
-      tail = templates.pop()
-      add(args, templates)
-      data.splice(i, 0, ...results)
-    } else {
-      args.push(chunks[i])
-    }
-  }
-  if (l === args.length && tail != null) args.push(tail + chunks[i])
-  return [args, ...data]
-}
-
-
-
-function add (array, chunks) {
-  if (chunks[0] === '') chunks.shift()
-  array.push(...chunks)
-}
-
-
-function transform (output) {
-  if (output == null) return [['']]
-  else if (output instanceof Array) {
-    return output
-  } else return [['', ''], output]
+  return [chunks, ...data]
 }
--- test/intertag.js
+++ test/intertag.js
@@ -6,88 +6,9 @@ const test = require('tape')
 const intersect = require('..')
 
 
-test('should be a tagged template', assert => {
+test('should be a regular tagged template', assert => {
   assert.plan(1)
   const bar = 'beep'
   const args = intersect`foo and ${bar}`
   assert.deepEqual(args, [['foo and ', ''], 'beep'])
 })
-
-
-test('should intersect two tagged template', assert => {
-  assert.plan(1)
-  const args = intersect`foo and ${john}`
-  assert.deepEqual(args, [['foo and john ', ''], 'doe'])
-})
-
-
-test('should intersect more than two tagged template', assert => {
-  assert.plan(1)
-  const args = intersect`${jane} and foo and ${john} and ${john} and ${jane}`
-  assert.deepEqual(args, [['', ' his sister and foo and john ', ' and john ', ' and ', ' his sister'], 'jane', 'doe', 'doe', 'jane'])
-})
-
-test('should work with imbricated interset', assert => {
-  assert.plan(1)
-  const args = intersect`${jane} and foo and ${complex} and ${jane} and ${john}`
-  assert.deepEqual(args, [ [ '', ' his sister and foo and beep and ', ' his sister and boop and ', ' his sister and john ', ''], 'jane', 'jane', 'jane', 'doe'])
-})
-
-
-test('should not intersect function returning null or undefined', assert => {
-  assert.plan(1)
-  const falsy = () => null
-  const args = intersect`foo and ${falsy}`
-  assert.deepEqual(args, [['foo and ']])
-})
-
-
-test('final intersect primitives', assert => {
-  assert.plan(1)
-  const boolean = () => true
-  const number = () => 2
-  const args = intersect`foo is ${number} years old ${boolean}`
-  assert.deepEqual(args, [['foo is ', ' years old ', ''], 2, true])
-})
-
-
-/**
- * Tagged templates partial.
- *
- * @return {Array}
- * @api private
- */
-
-function john () {
-  const who = 'doe'
-  return tagged`john ${who}`
-}
-
-
-/**
- * Tagged templates partial.
- *
- * @return {Array}
- * @api private
- */
-
-function jane () {
-  const who = 'jane'
-  return tagged`${who} his sister`
-}
-
-
-function complex () {
-  return intersect`beep and ${jane} and boop`
-}
-
-/**
- * Return tagged template.
- *
- * @return {Array}
- * @api privtate
- */
-
-function tagged (...args) {
-  return args
-}
